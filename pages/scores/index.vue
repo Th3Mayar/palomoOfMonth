21258924066816d76b29a27911ea754b3f5ff2d9
José Fco. Henriquez <108672422+Th3Mayar@users.noreply.github.com>
2025-07-13 01:57:00 +0000

    feat: :sparkles: adding new echart library for view charts to palomos

--- pages/scores/index.vue
+++ pages/scores/index.vue
@@ -2,10 +2,34 @@
     <div class="flex flex-col items-center justify-center h-screen">
         <h1 class="text-4xl font-bold mb-4">Scores Page</h1>
         <p class="text-lg mb-8">This page is under construction.</p>
+        <div id="main" style="width: 600px; height: 400px;"></div>
     </div>
 </template>
 
 <script setup>
+import * as echarts from 'echarts';
+
+const myChart = echarts.init(document.getElementById('main'));
+
+// Draw the chart
+myChart.setOption({
+  title: {
+    text: 'ECharts Getting Started Example'
+  },
+  tooltip: {},
+  xAxis: {
+    data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: 'sales',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+});
+
 // Page configuration
 definePageMeta({
   middleware: 'auth',
