f797d825172da7043a950f62c38641035027ee71
Th3Mayar <josehenriquez.02.26@gmail.com>
2025-07-17 09:37:07 -0400

    feat(dashboard): add main dashboard title and protect page with auth

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="text-2xl font-bold mb-6 mt-20 text-center">Dashboard</h1>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  middleware: 'auth',
+  layout: 'auth-layout'
+})
+</script>
+
+<style scoped></style>
